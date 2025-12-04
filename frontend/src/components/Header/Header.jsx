efebd0bab9765f0acdf0cbc64f96f24d8c8192de
jibankumarpanda <pandajiban331@gmail.com>
2025-12-04 12:05:24 +0530

    Your commit message here

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import './Header.css'
 const Header = () => {
     return (
         <div className='header'>
-            <div className='header-content'>
+            <div className='header-contents'>
                 <h2>
                     Order your favourite food here
                     <p1>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients to deliver an unforgettable taste experience right to your doorstep. </p1>
