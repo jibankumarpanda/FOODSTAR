1f9802f89399d8d15e73616aa928a209cfd191a4
jibankumarpanda <pandajiban331@gmail.com>
2025-12-05 11:46:17 +0530

    Fix: Update active menu item styling and fix useState import

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,16 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import StoreContextProvider from './Context/StoreContext';
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <StoreContextProvider>
       <App />
+    </StoreContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
