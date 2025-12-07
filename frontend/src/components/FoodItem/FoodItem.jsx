823550fd066a20dfca8925eb154d8060ada6a2ad
jibankumarpanda <pandajiban331@gmail.com>
2025-12-07 22:46:15 +0530

    Your commit message here

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -5,21 +5,40 @@ import { assets } from '../../assets/assets';
 const FoodItem = ({ id, name, price, description, image }) => {
     const [itemCount, setItemCount] = useState(0);
 
-
-return (
-    
+    return (
         <div className='food-item'>
-           <div className="food-item-img-container">
-                <img className='food-item-image' src={image} alt="" />
-           </div>
-           <div className="food-item-info">
+            <div className="food-item-img-container">
+                <img className='food-item-image' src={image} alt={name} />
+                {!itemCount 
+                    ? <img 
+                        className='add' 
+                        onClick={() => setItemCount(prev => prev + 1)}  
+                        src={assets.add_icon_white} 
+                        alt="Add to cart" 
+                      />
+                    : <div className='food-item-counter'>
+                        <img 
+                            onClick={() => setItemCount(prev => prev - 1)} 
+                            src={assets.remove_icon_red} 
+                            alt="Remove one" 
+                        />
+                        <p>{itemCount}</p>
+                        <img 
+                            onClick={() => setItemCount(prev => prev + 1)} 
+                            src={assets.add_icon_green} 
+                            alt="Add one more" 
+                        />
+                      </div>
+                }
+            </div>
+            <div className="food-item-info">
                 <div className="food-item-name-rating">
                     <p>{name}</p>
                     <img src={assets.rating_starts} alt="Rating" />
                 </div>
                 <p className="food-item-desc">{description}</p>
                 <p className="food-item-price">${price}</p>
-           </div>
+            </div>
         </div>
     );
 }
