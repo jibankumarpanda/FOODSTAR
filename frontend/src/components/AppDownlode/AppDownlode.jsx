a7d8817bdd9befb37f09fdd192c3f311d55a88fc
jibankumarpanda <pandajiban331@gmail.com>
2025-12-09 10:35:17 +0530

    Improved responsive design for mobile

---
+++ frontend/src/components/AppDownlode/AppDownlode.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './AppDownlode.css'
+import { assets } from '../../assets/assets'
+
+const AppDownlode = () => {
+    return (
+        <div className='app-downlode' id='app-downlode'>
+            <p>For Better Expeience Downlode<br /> FoodStar App</p>
+                <div className="app-downlode-platfor">
+                    <img src={assets.play_store} alt="" />
+                    <img src={assets.app_store} alt="" />
+                </div>
+        </div>
+    )
+}
+
+export default AppDownlode
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import './Home.css';
 import Header from '../../components/Header/Header';
 import ExploreMenu from '../../components/Exploremenu/ExploreMenu';
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay';
+import AppDownlode from '../../components/AppDownlode/AppDownlode';
 
 const Home = () => {
 
@@ -12,6 +13,7 @@ const Home = () => {
             <Header/>
             <ExploreMenu category={category} setCategory={setCategory}/>
             <FoodDisplay category={category}/>
+            <AppDownlode catagory={category}/>
         </div>
     )
 }
