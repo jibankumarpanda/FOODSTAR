434e5cf5e1cb3440d40fccc456312f55d859c664
jibankumarpanda <pandajiban331@gmail.com>
2025-12-11 19:51:40 +0530

    chore: update package.json and add MetaMask error handling

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, unstable_HistoryRouter as HistoryRouter } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import { StoreProvider } from './Context/StoreProvider';
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter
+      future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true
+      }}>
     <StoreProvider>
       <App />
     </StoreProvider>
